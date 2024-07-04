a2dbafcf5babb6cd6be9e62a4f482d15ddb55d0a
Atoleanantha <103057343+Atoleanantha@users.noreply.github.com>
2024-07-04 17:13:32 +0530

    custom api response and error handling ,utils code

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cookieParser from "cookie-parser"; //crud operation on user cookies
+import cors from "cors";
+
+const app=express();
+
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true
+}))
+
+app.use(express.json({limit:"16kb"})); //set limit to incomming json data to avoid server crashes
+app.use(express.urlencoded({extended:true,limit:"16kb"})); //encode url extended allow nested object
+app.use(express.static("public"));
+app.use(cookieParser());
+
+
+
+export default app
--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 // require('dotenv').config({path:"./env"});
 
 import dotenv from 'dotenv'
-import mongoose from "mongoose";
-import express from "express";
-const app=express();
+import app from './app.js';
 import connectDB from './db/index.js'
 dotenv.config({
     path:'./env'
 })
 
-connectDB();
+connectDB().then(()=>{
+    app.on("error",(error)=>{
+        console.log("ERROR: ",error);
+        throw error
+    })
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running at PORT: ${process.env.PORT}`);
+    })
+})
+.catch((error)=>{
+    console.log("MongoDB connection failed!!",error);
+});
 
 
 
